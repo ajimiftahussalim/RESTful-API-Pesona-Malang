81f380bdf3704af946c6ea0ae412f292e86acb07
Mohammad Faisal Ramdhani <88154279+faisalramdhani13@users.noreply.github.com>
2022-05-27 22:21:06 +0700

    add method patch (update)
    menambahkan route method patch (update) dan menambahkan import createTour

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { getTours } from "../controllers/tourController.js";
+import { getTours, createTour, updateTour } from "../controllers/tourController.js";
 
 const router = express.Router();
 
 router.get('/', getTours);
 router.post('/', createTour);
+router.patch('/:id', updateTour);
 
 export default router;
